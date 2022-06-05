d21893a182f11c229ae18a8226dbfdd9de66b996
takeshi-yaji <72482665+takeshi-yaji@users.noreply.github.com>
2022-06-05 14:49:34 +0900

    section4.nuxt-linkを使用する

---
+++ src/pages/news.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="">これはニュースです。</div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+
+@Component({})
+export default class extends Vue {
+  created() {
+    console.log(this.$route)
+  }
+}
+</script>
