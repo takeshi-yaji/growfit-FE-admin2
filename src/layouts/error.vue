af82ae59d1d6b9d755a07b0ec0bb164460e16c84
takeshi-yaji <72482665+takeshi-yaji@users.noreply.github.com>
2022-06-16 23:24:28 +0900

    section9.エラーページの作成とエラーハンドリング

---
+++ src/layouts/error.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex justify-center items-center h-screen">
+    <!-- エラーのステータスコードによって表示切り替え -->
+    <div v-if="error.statusCode === 404">
+      <span class="flex justify-center items-center text-3xl">404 not found</span>
+      <span class="flex justify-center items-center mt-4">ページが見つかりませんでした。</span>
+    </div>
+    <div v-else>
+      <span class="flex justify-center items-center text-3xl">500 internal server error</span>
+      <span class="flex justify-center items-center mt-4">サーバーにてエラーが発生しました。</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'nuxt-property-decorator'
+import { NuxtError } from '@nuxt/types'
+
+@Component({
+  layout: 'empty'
+})
+
+export default class extends Vue {
+  @Prop({ type: Object, required: false, default: () => {} })
+  error!: NuxtError
+
+  created() {
+    console.log(this.error);
+  }
+}
+</script>
--- src/pages/login.vue
+++ src/pages/login.vue
@@ -3,6 +3,9 @@
     <span class="flex justify-center text-lightGreen text-xl">Login Form</span>
     <BaseTextField class="mt-8" placeholder="ユーザー名"></BaseTextField>
     <BaseTextField class="mt-6" placeholder="パスワード"></BaseTextField>
+    <!-- loginミスがあった場合のエラーメッセージ -->
+    <span v-if="isError" class="flex justify-center items-center mt-6 text-red">ユーザー名またはパスワードが違います</span>
+
     <div class="flex justify-center items-center">
       <BaseButton class="mt-8" color="lightGreen" style="width: 200px" @click="onClickLogin">Login</BaseButton>
     </div>
@@ -17,6 +20,8 @@ import { Vue, Component } from 'nuxt-property-decorator'
 })
 export default class extends Vue {
   LoginRepository = this.$repositories.login
+  // エラー状態のフラグ
+  isError: boolean = false
   email: string = ''
   password: string = ''
 
@@ -25,9 +30,15 @@ export default class extends Vue {
       email: this.email,
       password: this.password,
     }
-    const res = await this.LoginRepository.post(params)
-    localStorage.setItem('access_token', res.access_token)
-    this.$router.push('/')
+
+    const res = await this.LoginRepository.post(params).catch(() => {
+      this.isError = true
+    })
+
+    if (res?.access_token) {
+      localStorage.setItem('access_token', res.access_token)
+      this.$router.push('/')
+    }
   }
 }
 </script>
